09f7f542300bf25d9652c3af2946134312b3f196
blossommaggi <55086540+blossommaggi@users.noreply.github.com>
2019-09-10 15:14:16 +0530

    Add files via upload

---
+++ bigsmall.py
@@ -0,0 +1,13 @@
+n=int(input())
+x=[int(x) for x in input().split()]
+print(x)
+big=low=x[0]
+bi=li=0
+for i in range(0,n):
+    if(x[i]>=big):
+        big=x[i]
+        bi=i+1
+    if(x[i]<=low):
+         low=x[i]
+         li=i+1
+print(li,bi)
