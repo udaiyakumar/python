1f9f26d55fea0608fdf3e813365939f059294290
blossommaggi <55086540+blossommaggi@users.noreply.github.com>
2019-09-10 10:41:55 +0530

    Update arraybit.py

--- arraybit.py
+++ arraybit.py
@@ -1,6 +1,7 @@
 n=int(input())
-x=[int(x) for x in input().split()]
-res=x[0]
-for i in range(n):
-    res=res&x[i]
-print(res)
+if(n<=100000):
+    x=[int(x) for x in input().split()]
+    res=x[0]
+    for i in range(n):
+        res=res&x[i]
+    print(res)
